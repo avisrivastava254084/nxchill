5f3d3c1c71cb8a4c3be15793d46f6d6f6ac5d9fd
Mohit <mohitaquarian97@gmail.com>
2018-05-18 20:09:34 +0530

    Flask interface added with templates.

---
+++ nxchill.py
@@ -0,0 +1,22 @@
+from flask import flask
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+import owner as o
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	render_template('Welcome.html')
+	pass
+@app.route('/register')
+def register():
+	render_template('Register.html') 
+@app.route('/registered', methods = ['POST'])
+def registered():
+	email = request.form['Email']
+	scr = request.form['scr']
+	ascr = request.form['ascr']
+	msg = request.form['msg']
+	o.owner(email,scr,ascr,msg)
+	o.register()
+	return home()
+@app
